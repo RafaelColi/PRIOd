1c180471d5db7426c6d5ad4852538d4d9f01c6a2
RafaelColi <rafacolialves@gmail.com>
2023-10-24 17:49:48 -0300

    Correcoes para ciclo de clock

--- PRIOd.c
+++ PRIOd.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <strings.h>
 #include "tarefa.h"
 #include "lista_tarefas.h"
 
 typedef struct cpu {
     int clock;
     int concluido;
+    int prio;
 } cpu;
 
 cpu* criar_cpu() {
@@ -14,7 +16,19 @@ cpu* criar_cpu() {
     return nova;
 }
 
-void ingressar(lista_tarefas* tarefas, lista_tarefas* ingressadas, cpu* cpu) {
+tarefa* proxima_a_executar(lista_tarefas* ingressadas, cpu* cpu, lista_tarefas* tarefas, lista_tarefas* concluidas) {
+    int prio_max = 0;
+
+    for(int i = 0; i < ingressadas->tam; i++) {
+        tarefa* tarefa = ingressadas->tarefas[i];
+
+        if(tarefa->priod > ingressadas->tarefas[prio_max]->priod) {
+            prio_max = i;
+        }
+    }
+}
+
+void ingressar(lista_tarefas* tarefas, lista_tarefas* ingressadas, cpu* cpu, lista_tarefas* concluidas) {
     for(int i = 0; i < tarefas->tam; i++) {
         tarefa* pronta = tarefas->tarefas[i];
 
@@ -26,9 +40,9 @@ void ingressar(lista_tarefas* tarefas, lista_tarefas* ingressadas, cpu* cpu) {
     return;
 }
 
-void priod(lista_tarefas* ingressadas, cpu* cpu, lista_tarefas* tarefas) {
-    ingressar(tarefas, ingressadas, cpu);
-    
+void priod(lista_tarefas* ingressadas, cpu* cpu, lista_tarefas* tarefas, lista_tarefas* concluidas) {
+    ingressar(tarefas, ingressadas, cpu, concluidas);
+
     int prio_max = 0;
 
     for(int i = 0; i < ingressadas->tam; i++) {
@@ -45,13 +59,22 @@ void priod(lista_tarefas* ingressadas, cpu* cpu, lista_tarefas* tarefas) {
         if(i == prio_max) {
             executar(tarefa);
 
+            printf("Tarefa executada no clock %d\n", cpu->clock);
+            printf("Tarefa %s -> Prioridade: %d, Tempo restante: %d, Tempo de espera: %d\n", tarefa->id, tarefa->priod, tarefa->restante, tarefa->tempo_espera);
+
             if(tarefa->restante == 0) {
                 cpu->concluido++;
+                add_tarefa(tarefa, concluidas);
                 remover_tarefa(tarefa, ingressadas);
+
+                printf("Tarefa concluida no clock %d\n", cpu->clock + 1);
+                printf("Tarefa %s -> Prioridade: %d, Tempo restante: %d, Tempo de espera: %d\n", tarefa->id, tarefa->priod, tarefa->restante, tarefa->tempo_espera);
             }
 
         } else {
             elevar_prioridade(tarefa);
+            printf("Tarefa em espera no clock %d\n", cpu->clock);
+            printf("Tarefa %s -> Prioridade: %d, Tempo restante: %d, Tempo de espera: %d\n", tarefa->id, tarefa->priod, tarefa->restante, tarefa->tempo_espera);
         }
     }
 }
@@ -63,12 +86,13 @@ int main() {
 
     lista_tarefas* tarefas = criar_lista_tarefas(n_tarefas);
     lista_tarefas* ingressadas = criar_lista_tarefas(n_tarefas);
+    lista_tarefas* concluidas = criar_lista_tarefas(n_tarefas);
 
-    tarefa* t1 = criar_tarefa(0, 4, 1);
-    tarefa* t2 = criar_tarefa(0, 1, 4);
-    tarefa* t3 = criar_tarefa(1, 5, 2);
-    tarefa* t4 = criar_tarefa(3, 2, 3);
-    tarefa* t5 = criar_tarefa(5, 3, 5);
+    tarefa* t1 = criar_tarefa("t1", 0, 4, 1);
+    tarefa* t2 = criar_tarefa("t2", 0, 1, 4);
+    tarefa* t3 = criar_tarefa("t3", 1, 5, 2);
+    tarefa* t4 = criar_tarefa("t4", 3, 2, 3);
+    tarefa* t5 = criar_tarefa("t5", 5, 3, 5);
 
 
     add_tarefa(t1, tarefas);
@@ -78,7 +102,7 @@ int main() {
     add_tarefa(t5, tarefas);
 
     while(cpu->concluido < tarefas->tam) {
-        priod(ingressadas, cpu, tarefas);
+        priod(ingressadas, cpu, tarefas, concluidas);
         cpu->clock++;
     }
     
